0ca3b0a7d4678e7ff4ed99c908b5671e24457d70
tu-mera-dusman <you@example.com>
2025-03-03 16:45:44 +0530

    nobita

---
+++ Branded/plugins/tools/all_tag.py
@@ -0,0 +1,109 @@
+from ... import *
+from pyrogram import filters
+
+
+
+import asyncio
+
+from pyrogram import filters
+from pyrogram.enums import ChatMembersFilter
+from pyrogram.errors import FloodWait
+
+SPAM_CHATS = []
+
+@app.on_message(
+    filters.command(["king"], prefixes=["."])
+     & ~filters.private
+)
+async def tag_all_users(_, message):
+
+    replied = message.reply_to_message
+    if len(message.command) < 2 and not replied:
+        await message.reply_text(
+            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@all Hi Friends`"
+        )
+        return
+    if replied:
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
+        usertxt = ""
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
+                break
+            usernum += 1
+            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            if usernum == 1:
+                await replied.reply_text(usertxt)
+                await asyncio.sleep(2)
+                usernum = 0
+                usertxt = ""
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
+    else:
+        text = message.text.split(None, 1)[1]
+
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
+        usertxt = ""
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
+                break
+            usernum += 1
+            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            if usernum == 1:
+                await app.send_message(
+                    message.chat.id,
+                    f"{text}\n{usertxt}\n\n|| # 𝚲 𝑳 𝜩 𝛸 𝙐 ™` »  ||",
+                )
+                await asyncio.sleep(2)
+                usernum = 0
+                usertxt = ""
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
+
+
+@app.on_message(
+    filters.command(
+        [
+            "stopmention",
+            "offall",
+            "cancel",
+            "allstop",
+            "stopall",
+            "cancelmention",
+            "offmention",
+            "mentionoff",
+            "alloff",
+            "cancelall",
+            "allcancel",
+        ],
+        prefixes=[".","#"],
+    )
+    & ~filters.private
+)
+async def cancelcmd(_, message):
+    chat_id = message.chat.id
+    if chat_id in SPAM_CHATS:
+        try:
+            SPAM_CHATS.remove(chat_id)
+        except Exception:
+            pass
+        return await message.reply_text("**ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")
+
+    else:
+        await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")
+        return
+
+
+__NAME__ = "king"
+__MENU__ = """
+**king the members one by one
+Or Group By Simple Commands.**
+
+`.king` - text/reply ke chat.
+`.kingoff` - to stop .kingoff.
+"""
